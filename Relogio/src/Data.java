3a7258aa1adfc9a4f67888d1f5f079a51ef3c0f4
TeofiloFreitas <59712317+TeofiloFreitas@users.noreply.github.com>
2020-09-08 15:20:17 -0300

    Upload file

---
+++ Relogio/src/Data.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Data {
+
+	public static void main(String[] args) {
+		
+		Date relogio = new Date();
+		
+		System.out.println("A hora do sistema é " + relogio.toString());
+
+	}
+
+}
